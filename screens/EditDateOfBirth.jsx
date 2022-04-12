13fa7a9b5fbd1d79126b5316c118e75997074702
Terry Amagboro <rukkiecodes@gmail.com>
2022-04-12 23:18:37 +0100

    update gender

--- screens/EditDateOfBirth.jsx
+++ screens/EditDateOfBirth.jsx
@@ -1,8 +1,7 @@
-import { View, Text, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard, TouchableOpacity, TextInput } from 'react-native'
+import { View, Text, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard, TouchableOpacity } from 'react-native'
 import React from 'react'
 import DatePicker from 'react-native-datepicker';
 
-import SimpleLineIcons from "react-native-vector-icons/SimpleLineIcons"
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
 
 import Bar from "./StatusBar"
@@ -11,71 +10,120 @@ import editProfile from '../style/editProfile'
 
 import useAuth from "../hooks/useAuth"
 
+import { useFonts } from 'expo-font'
+import color from '../style/color'
+
 const EditDateOfBirth = () => {
   const { updateDateState, updateDateOfBirth } = useAuth()
+
+  const [loaded] = useFonts({
+    text: require("../assets/fonts/Montserrat_Alternates/MontserratAlternates-Medium.ttf")
+  })
+
+  if (!loaded)
+    return null
 
   return (
     <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={editProfile.container}>
       <Bar />
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={{ backgroundColor: "#fff", flex: 1 }}>
-          <View style={editProfile.header}>
-            <View style={editProfile.left}>
-              <TouchableOpacity onPress={() => navigation.goBack()}>
-                <SimpleLineIcons name="arrow-left" color="rgba(0,0,0,0.8)" size={20} />
+        <>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingHorizontal: 10,
+              height: 45
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <TouchableOpacity
+                onPress={() => navigation.goBack()}
+                style={{
+                  width: 40,
+                  height: 40,
+                  justifyContent: "center",
+                  alignItems: "center"
+                }}
+              >
+                <MaterialCommunityIcons name='chevron-left' color={color.dark} size={30} />
               </TouchableOpacity>
-              <Text style={editProfile.headText}>Date of birth</Text>
+              <Text
+                style={{
+                  marginLeft: 10,
+                  fontSize: 18,
+                  fontFamily: "text"
+                }}
+              >
+                Date of birth
+              </Text>
             </View>
-            <TouchableOpacity onPress={updateDateOfBirth}>
-              <MaterialCommunityIcons name="check" color="#4169e1" size={24} />
+            <TouchableOpacity
+              onPress={updateDateOfBirth}
+              style={{
+                width: 40,
+                height: 40,
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <MaterialCommunityIcons name="check" color={color.dark} size={24} />
             </TouchableOpacity>
           </View>
-
-          <View style={editProfile.form}>
-            <View style={{
-              height: 45,
-              marginBottom: 30,
-              position: "relative"
-            }}>
-              <Text style={{ fontSize: 12, color: "rgba(0,0,0,0.4)" }}>Select Date of birth</Text>
-              <DatePicker
-                style={{
-                  width: "100%",
-                }}
-                date={updateDateState.date}
-                mode="date"
-                placeholder="select date"
-                format="DD/MM/YYYY"
-                confirmBtnText="Confirm"
-                cancelBtnText="Cancel"
-                customStyles={{
-                  dateIcon: {
-                    position: 'absolute',
-                    right: -5,
-                    top: 4,
-                    marginLeft: 0,
-                  },
-                  dateInput: {
-                    borderColor: "gray",
-                    alignItems: "flex-start",
-                    borderWidth: 0,
-                    borderBottomWidth: 1,
-                  },
-                  placeholderText: {
-                    fontSize: 16,
-                    color: "gray"
-                  },
-                  dateText: {
-                    fontSize: 16,
-                  }
-                }}
-                onDateChange={(date) => {
-                  updateDateState.setDate(date);
-                }}
-              />
+          <View style={{ backgroundColor: "#fff", flex: 1 }}>
+            <View style={editProfile.form}>
+              <View style={{
+                height: 45,
+                marginBottom: 30,
+                position: "relative"
+              }}>
+                <Text style={{ fontSize: 12, color: "rgba(0,0,0,0.4)" }}>Select Date of birth</Text>
+                <DatePicker
+                  style={{
+                    width: "100%",
+                  }}
+                  date={updateDateState.date}
+                  mode="date"
+                  placeholder="select date"
+                  format="DD/MM/YYYY"
+                  confirmBtnText="Confirm"
+                  cancelBtnText="Cancel"
+                  customStyles={{
+                    dateIcon: {
+                      position: 'absolute',
+                      right: -5,
+                      top: 4,
+                      marginLeft: 0,
+                    },
+                    dateInput: {
+                      borderColor: "gray",
+                      alignItems: "flex-start",
+                      borderWidth: 0,
+                      borderBottomWidth: 1,
+                    },
+                    placeholderText: {
+                      fontSize: 16,
+                      color: "gray"
+                    },
+                    dateText: {
+                      fontSize: 16,
+                    }
+                  }}
+                  onDateChange={(date) => {
+                    updateDateState.setDate(date);
+                  }}
+                />
+              </View>
             </View>
           </View>
-        </View>
+        </>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
   )
